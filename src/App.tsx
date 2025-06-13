3e2cd0a0e4fb23933ff5a1e4f5173a3e14ec07e3
ashishkunthe <ashishkunthe2003@gmail.com>
2025-06-13 21:02:00 +0530

    routes changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Menu from "./pages/Menu";
 import Welcome from "./pages/Welcome";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -11,7 +12,15 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/menu" element={<Menu />} />
+
+        <Route
+          path="/menu"
+          element={
+            <ProtectedRoute>
+              <Menu />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
